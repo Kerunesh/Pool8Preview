a9f6481ea4894c624a73dc833e68abeed7917834
foxstudent101476 <axetnm@gmail.com>
2025-09-03 11:04:43 +0300

    fixed harcode + added cue lerp
    refartored code, addet lerp for cue while strike

--- Assets/Game/Scripts/CueControllerScript.cs
+++ Assets/Game/Scripts/CueControllerScript.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System.Security;
 
 public class CueControllerScript : MonoBehaviour
 {
     public Rigidbody cueBallRigidbody;
     public GameObject cueGraphics;
 
-    [SerializeField] float strikeForce = 10f;
-    
+    [SerializeField] float minStrikeForce = 1.0f;
+    [SerializeField] float maxStrikeForce = 10f;
+    [SerializeField] float forceChangeSpeed = 5f;
+    [SerializeField] float maxPullBackDistance = 4f;
+    [SerializeField] private float ballStopThreshold = 0.01f;
+
+    private float currentStrikeForce;
+    private Coroutine chargeShotCoroutine;
+    private bool isAiming = true;
+
+
     private List<Rigidbody> allBallsRigidbodies;
 
     // Start is called before the first frame update
@@ -22,13 +32,22 @@ public class CueControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (cueGraphics.activeSelf)
+        if (isAiming)
         {
             Aim();
 
             if (Input.GetMouseButtonDown(0))
             {
-                Strike();
+                chargeShotCoroutine = StartCoroutine(ChargeShotCoroutine());
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (chargeShotCoroutine != null)
+                {
+                    StopCoroutine(chargeShotCoroutine);
+                    Strike();
+                }
             }
         }
                 
@@ -38,22 +57,46 @@ public class CueControllerScript : MonoBehaviour
     {
         Vector3 cueBallScreenPos = Camera.main.WorldToScreenPoint(cueBallRigidbody.transform.position);
         Vector3 mousePos = Input.mousePosition;
-        Vector2 direction = new Vector2 (mousePos.x - cueBallScreenPos.x, mousePos.y - cueBallScreenPos.y);
+        Vector2 direction = new Vector2(mousePos.x - cueBallScreenPos.x, mousePos.y - cueBallScreenPos.y);
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
     }
+    
 
         void Strike()
     {
+        isAiming = false;
         SetCueVisibility(false);
 
-        var strikeDirection = -transform.right;
+        cueGraphics.transform.localPosition = Vector3.zero;
 
-        cueBallRigidbody.AddForce(strikeDirection * strikeForce, ForceMode.Impulse);
+        var strikeDirection = -transform.right;
+        cueBallRigidbody.AddForce(strikeDirection * maxStrikeForce, ForceMode.Impulse);
 
         StartCoroutine(CheckIfBallStoppedRoutine());
+    }
+
+    private IEnumerator ChargeShotCoroutine()
+    {
+        float chargeTime = 0f;
+
+        while (true)
+        {
+            float pingPongValue = Mathf.PingPong(chargeTime * forceChangeSpeed, 1f);
+
+            currentStrikeForce = Mathf.Lerp(minStrikeForce, maxStrikeForce, pingPongValue);
+
+            float pullBackValue = Mathf.Lerp(0, maxPullBackDistance, pingPongValue);
+
+            Vector3 cuePosition = cueGraphics.transform.localPosition;
+            cuePosition.x = pullBackValue;
+            cueGraphics.transform.localPosition = cuePosition;
+
+            chargeTime += Time.deltaTime;
+            yield return null;
+        }
     }
 
     IEnumerator CheckIfBallStoppedRoutine()
@@ -67,7 +110,7 @@ public class CueControllerScript : MonoBehaviour
             {
                 if (ballRB != null)
                 {
-                    if (ballRB.velocity.magnitude > 0.01f || ballRB.angularVelocity.magnitude > 0.01f)
+                    if (ballRB.velocity.magnitude > ballStopThreshold || ballRB.angularVelocity.magnitude > ballStopThreshold)
                     {
                         allStopped = false;
                         break;
@@ -84,7 +127,8 @@ public class CueControllerScript : MonoBehaviour
             yield return new WaitForFixedUpdate();
         }
 
-        Debug.Log("Все шары остановились. Можно бить.");
+        Debug.Log("All balls are stopped. You can strike now.");
+        isAiming = true;
         SetCueVisibility(true);
 
     }
